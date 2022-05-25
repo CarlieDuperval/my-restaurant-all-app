30d8e392969a8689d024a0c93db0cff8714445b5
Carlie Duperval <carlierichell@gmail.com>
2022-05-25 14:54:28 -0400

    Added update

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  useEffect(async () => {
+    const response = await fetch('https://my-first-firestore-cd.web.app/restaurants/')
+    const data = await response.json()
+    console.log(data)
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi Carlie whats up!</Text>
       <StatusBar style="auto" />
     </View>
   );
